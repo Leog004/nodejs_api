6e668b4bbaa1e5b886388285c5c56c4a4988f97b
Leo Garza <lgarza@equibrand.com>
2022-03-21 15:13:09 -0500

    doc

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
 
@@ -10,13 +13,29 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1). MIDDLEWARE
+// Set Security HTTP headers
+app.use(helmet());
+
+// Logging MIDDLEWARE
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// limits the amount of request made per api
+const limiter = rateLimit({
+  max: 100,
+  windowMS: 60 * 60 * 1000,
+  message: 'To many request from this ip.',
+});
+app.use('/api', limiter);
+
+// Body Parser, reading data from req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public/`));
 
+// Time Logging
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
