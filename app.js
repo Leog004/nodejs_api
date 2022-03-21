79a6ae4c5803bd44b2200ae98f4b1517478655ac
Leo Garza <lgarza@equibrand.com>
2022-03-21 15:25:04 -0500

    security measures

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
@@ -32,6 +35,23 @@ app.use('/api', limiter);
 // Body Parser, reading data from req.body
 app.use(express.json({ limit: '10kb' }));
 
+// Data sanitizer against noSQL injection
+app.use(mongoSanitize());
+// Data sanitizer against XSS
+app.use(xss());
+// Prevents url polution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 // Serving static files
 app.use(express.static(`${__dirname}/public/`));
 
