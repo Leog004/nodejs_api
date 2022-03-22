8b60ffec5979a44c8f06ce359d7c20c85f9d2837
Leo Garza <lgarza@equibrand.com>
2022-03-22 14:47:05 -0500

    authcontroller

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
@@ -35,6 +36,7 @@ app.use('/api', limiter);
 
 // Body Parser, reading data from req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanitizer against noSQL injection
 app.use(mongoSanitize());
