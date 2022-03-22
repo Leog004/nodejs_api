762098f262b211bfb4f6e1b1d868b9b00b22c5a0
Leo Garza <lgarza@equibrand.com>
2022-03-22 16:13:41 -0500

    tour file upload

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const multer = require('multer');
 const userController = require('../controller/userController');
 const authController = require('../controller/authController');
 
