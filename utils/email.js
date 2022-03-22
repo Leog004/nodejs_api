e6ee00a3b87b1114b92282a49984e01e187b489d
Leo Garza <lgarza@equibrand.com>
2022-03-22 16:37:13 -0500

    Email class

--- utils/email.js
+++ utils/email.js
@@ -1,24 +1,54 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const { htmlToText } = require('html-to-text');
 
-const sendEmail = async (options) => {
-  //1). Create a transporterver
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  //2). Define the email options
-  const mailOptions = {
-    from: 'Leo Garza <leo@leo.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
-  //3). Actually send the email
-  await transporter.sendMail(mailOptions);
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = 'STORE NAME <store@store.com>';
+  }
+
+  createTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // SendGrid
+      return 1;
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    // 1) Render HTML based on pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject: html,
+      text: htmlToText(html),
+    };
+    // 3) Create a transport and send email
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  sendWelcome() {
+    this.send('Welcome', 'Welcome to store!');
+  }
 };
-
-module.exports = sendEmail;
